0dac37d7abf7b2c3b93991e1e65aed5a595859e5
Roman David Carignano <romandavidcarignano@gmail.com>
2023-11-18 11:09:43 -0300

    Borrado

--- Cubits-Web/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue';
-import PrimeVue from 'primevue/config';
-import 'primevue/resources/themes/lara-light-teal/theme.css';
-
-createApp(App).use(PrimeVue).mount('#app')
